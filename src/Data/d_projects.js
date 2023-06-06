c1251fc8fc2780c8973d888b7e34723ba6632268
GauravS43 <gaurav53080@gmail.com>
2023-06-06 18:39:39 -0400

    added rough project overviews

--- src/Data/d_projects.js
+++ src/Data/d_projects.js
@@ -1,12 +1,14 @@
-import sf_gif from "../Components/Images/trainedSample.gif"
-import pf_gif from "../Components/Images/PF_run.gif"
-import man_gif from "../Components/Images/runthrough.gif"
+import sf_pic from "../Components/Images/sf_vs.png"
+import pf_pic from "../Components/Images/pf_run.png"
+import man_pic from "../Components/Images/manga.png"
+import trig_pic from "../Components/Images/trig_test.png"
 
 /*
 let sample_project = [
     title,
     date,
     text,
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc luctus turpis vitae accumsan condimentum. Praesent congue vel quam in blandit. Morbi in nibh elementum, aliquet libero eget, condimentum risus. Donec sagittis, sem nec volutpat sodales, tortor quam venenatis turpis, vitae fringilla elit lectus et lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam id ornare justo, et suscipit erat. Praesent sodales nec magna fringilla fringilla. Maecenas interdum ante quis massa sagittis, eu molestie magna molestie. Morbi porta pharetra pretium. Phasellus eu mi dictum, vestibulum lectus nec, sagittis est. Curabitur vitae eleifend elit."
     link title,
     link URL,
 ]
@@ -15,37 +17,37 @@ let sample_project = [
 let SF_RL = [
     "Street Fighter Reinforcement Learning",
     "05/23",
-    "Developed a workflow to preprocess, tune, and train a model for Street Fighter 2 Special Champion Edition (Sega Genesis) in Python using Gym, Stable-Baselines3, and Gym Retro. The model was trained using Proximal Policy Optimization for 5,000,000 steps using in-game score as a reward function. At its peak, the model is capable of winning 9 matches consecutively or reach up to Dhalsim in Ryu's arcade mode.",
+    "Hadouken! Developed a workflow to preprocess, tune, and train a model for Street Fighter 2 Special Champion Edition (Sega Genesis) in Python using Gym, Stable-Baselines3, and Gym Retro. The model was trained using Proximal Policy Optimization for 5,000,000 steps using in-game score as a reward function. At its peak, the model is capable of winning 9 matches consecutively or reach up to Dhalsim in Ryu's arcade mode.",
     "Github",
     "https://github.com/GauravS43/Street-Fighter-Reinforcement-Learning",
-    sf_gif
+    sf_pic
 ]
 
 let PF_VIS = [
     "Path Finding Visualizer",
     "08/22",
-    "Developed a visualizer for seven pathfinding algorithms using React. ",
+    "Compare different path finding algorithms in this website. This interactive visualizer featuring seven pathfinding algorithms was developed independently by me using React. The visualizer includes Dijkstra, A Star (A*), Depth First Search (DFS), Breadth First Search (BFS), and other similar algorithms. The user can also generates mazes, place walls, or manipulate weights to showcase the different strengths and weaknesses of each algorithm.",
     "Github",
     "https://github.com/GauravS43/PathFindingVisualizer",
-    pf_gif
+    pf_pic
 ]
 
 let MAN_HL = [
     "Manga Higher or Lower",
     "07/22",
-    "",
+    "Test your Manga Knowledge in this website. Choose which of two randomly selected manga has the higher popularity or rating based on MyAnimeList users. This web-based app utlizes a custom API built in Python using Flask that stores information on 2000 manga. The front-end was independently designed and built based on the Higher Lower Game.",
     "Github",
     "https://github.com/GauravS43/MangaHigherLower",
-    man_gif
+    man_pic
 ]
 
 let TRIG_APP = [
     "Trigonometry Helper App",
     "02/22 - 04/22",
-    "",
+    "A multi-page web application on trigonometry developed with a team of 4 other students. Independently developed the quiz section which generates diagrams to enhance user engagement and learning",
     "Github",
     "https://github.com/GauravS43/Trigonometry-Helper",
-    pf_gif
+    trig_pic
 ]
 
 let MARIO_FPS = [
@@ -54,7 +56,7 @@ let MARIO_FPS = [
     "",
     "Video",
     "",
-    sf_gif
+    sf_pic
 ]
 
 
