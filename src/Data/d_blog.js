ba2f58f0eb8704c46ab6ec6e2f546982d8e0193f
GauravS43 <gaurav53080@gmail.com>
2023-06-20 21:19:30 -0400

    edited blog posts

--- src/Data/d_blog.js
+++ src/Data/d_blog.js
@@ -1,13 +1,13 @@
 let website_post = [
     "Making a website!",
     "06/23",
-    "Creating a website, and by extension a blog, can be an intimidating endeavor. It requires consistent maintenance and updates, as well as the dedication to share new content regularly. But it is for this reason that I believe that building a website is a rewarding experience. In this blog post, I will delve into my recent journey of creating a website using React, reflecting on past projects, and exploring the exciting possibilities that lie ahead."
+    "Creating a website and a blog can be an intimidating endeavour. It requires consistent updates alongside the dedication to share new content regularly. But it is for this reason that I believe that building a website is a rewarding experience. In this blog post, I will delve into my recent journey of creating a website using React, reflecting on past projects, and exploring exciting possibilities in the future."
 ]
 
 let sf_rl_post = [
     "Reinforcement Learning SF2",
     "06/23",
-    "As my first machine learning project, I wanted to approach the material in a way that would ignite and sustain my interest in the subject for the duration of the project. Inspired by Youtube videos of machine learning models learning and eventually beating retro video games, I knew I had found the perfect project. In this blog post, I will discuss my inspirations for this project, the process of learning machine learning, and the difficulties in training the model."
+    "As my first machine learning project, I wanted to approach the material in a way that would ignite and sustain my interest in the subject for the duration of the project. Inspired by Youtube videos of machine learning models learning and eventually beating retro video games, I knew I had found the perfect project. In this blog post, I will discuss my inspirations for this project, learning machine learning, and the difficulties in training the model."
 ]
 
 export { website_post, sf_rl_post }
